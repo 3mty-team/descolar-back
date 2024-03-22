09c5a373397d4395c69bfe8821c903792438b96d
BadiiiX <contact@mehdi-ali.me>
2024-03-22 16:20:50 +0100

    DB-54 ✨[FEAT] Changing Handler Type for Whoops

--- App/Adapters/Error/ErrorManager.php
+++ App/Adapters/Error/ErrorManager.php
@@ -2,19 +2,39 @@
 
 namespace Descolar\Adapters\Error;
 
+use Descolar\App;
 use Descolar\Managers\Error\Interfaces\IErrorManager;
 use Override;
+use Whoops\Handler\Handler;
+use Whoops\Handler\JsonResponseHandler;
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
 
 class ErrorManager implements IErrorManager
 {
 
+    private function selectHandler(): Handler
+    {
+        if (App::getEnvManager()->get('env') === 'DEV') {
+            $handler = new PrettyPageHandler;
+            $handler->setPageTitle("Descolar Error");
+            $handler->setEditor("idea");
+        } else {
+            $handler = new JsonResponseHandler;
+            $handler->setJsonApi(true);
+        }
+
+        return $handler;
+    }
+
     #[Override] public function manage(): void
     {
         $whoops = new Run;
-        $whoops->pushHandler(new PrettyPageHandler);
+        $handler = $this->selectHandler();
+
+        $whoops->pushHandler($handler);
         $whoops->register();
+
     }
 
 }
