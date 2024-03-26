cb733a141fb10c89d3d097ec4496bcab2783ed77
BadiiiX <contact@mehdi-ali.me>
2024-03-26 12:27:45 +0100

    DB-54 ✨[DRAFT] Setting up http error code

--- App/Adapters/Error/ErrorManager.php
+++ App/Adapters/Error/ErrorManager.php
@@ -2,37 +2,43 @@
 
 namespace Descolar\Adapters\Error;
 
+use _PHPStan_6b522806f\Nette\Neon\Exception;
+use Descolar\Adapters\Error\Handlers\CustomHandler;
 use Descolar\App;
 use Descolar\Managers\Error\Interfaces\IErrorManager;
 use Override;
-use Whoops\Handler\Handler;
 use Whoops\Handler\JsonResponseHandler;
 use Whoops\Handler\PrettyPageHandler;
+use Whoops\Inspector\InspectorFactory;
 use Whoops\Run;
 
 class ErrorManager implements IErrorManager
 {
 
-    private function selectHandler(): Handler
+    private function selectHandler(): CustomHandler
     {
+        $inspector = (new InspectorFactory())->create(new Exception());
         if (App::getEnvManager()->get('env') === 'DEV') {
-            $handler = new PrettyPageHandler;
-            $handler->setPageTitle("Descolar Error");
-            $handler->setEditor("idea");
+            $handler = new CustomHandler(new PrettyPageHandler);
+            $handler->getHandler()->setPageTitle("Descolar Error");
+            $handler->getHandler()->setEditor("idea");
         } else {
-            $handler = new JsonResponseHandler;
-            $handler->setJsonApi(true);
+            $handler = new CustomHandler(new JsonResponseHandler);
         }
 
+        $handler->setInspector($inspector);
         return $handler;
     }
 
     #[Override] public function manage(): void
     {
         $whoops = new Run;
+
         $handler = $this->selectHandler();
 
-        $whoops->pushHandler($handler);
+        $whoops->pushHandler($handler->getHandler());
+
+        $whoops->sendHttpCode($handler->getException()->getCode());
         $whoops->register();
 
     }
